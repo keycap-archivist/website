c8b46f35dc58e90dc393acdb7198896370e2211f
Vincent LE GOFF <vince.legoff@gmail.com>
2020-05-13 22:40:00 +0200

    feat : add sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,13 @@ module.exports = {
     title: 'Keycap Archivist',
     description: '',
     author: '@keycap-archivist',
+    siteUrl: 'https://keycap-archivist.github.io/website/',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-postcss',
+    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -27,6 +30,17 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import '../assets/css/base.css';
+import '../assets/scss/style.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
