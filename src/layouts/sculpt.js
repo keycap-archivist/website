fc6edfc310f322643ec6500ccc82f64e04e74dc1
Nick Christus <nick.christus@icloud.com>
2020-05-20 10:56:36 -0500

    ui_cleanup: removed catalog db

--- src/layouts/sculpt.js
+++ src/layouts/sculpt.js
@@ -5,14 +5,20 @@ import Layout from '../components/layout';
 const Maker = (props) => {
   const { pageContext } = props;
   const { sculpt } = pageContext;
+  const { maker } = pageContext;
+
+  console.log(pageContext);
+
   return (
     <Layout>
-      <h1>{sculpt.name}</h1>
-      <ul>
+      <h2 className="text-3xl mb-3">
+        {maker.name} / <span className="font-bold">{sculpt.name}</span>
+      </h2>
+      <ul className="flex flex-row flex-wrap w-full md:-mx-2">
         {sculpt.colorways.map((c) => (
-          <li key={c.id}>
+          <li key={c.id} className="md:w-1/4 lg:w-1/6 py-3 md:px-2">
+            <img className="block max-w-full" src={c.img} />
             {c.name}
-            <img style={{ maxWidth: '250px' }} src={c.img} />
           </li>
         ))}
       </ul>
