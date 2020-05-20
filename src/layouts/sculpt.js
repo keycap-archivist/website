4eebfa73dd54cab9ddaa863ac9f4da23ba5055bd
Vincent LE GOFF <vince.legoff@gmail.com>
2020-05-20 20:10:51 +0200

    feat: improve sculpts (#7)
    * feat: improve sculpts
    
    * lint

--- src/layouts/sculpt.js
+++ src/layouts/sculpt.js
@@ -1,22 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const Maker = (props) => {
   const { pageContext } = props;
-  const { sculpt } = pageContext;
-  const { maker } = pageContext;
-
-  console.log(pageContext);
+  const { maker, makerUrl, sculpt } = pageContext;
 
   return (
     <Layout>
       <h2 className="text-3xl mb-3">
-        {maker.name} / <span className="font-bold">{sculpt.name}</span>
+        <Link to={makerUrl}>{maker.name}</Link> / <span className="font-bold">{sculpt.name}</span>
       </h2>
       <ul className="flex flex-row flex-wrap w-full md:-mx-2">
         {sculpt.colorways.map((c) => (
-          <li key={c.id} className="md:w-1/4 lg:w-1/6 py-3 md:px-2">
+          <li key={c.id} className="md:w-1/4 lg:w-1/6 py-3 md:px-2 text-center">
             <img className="block max-w-full" src={c.img} />
             {c.name}
           </li>
