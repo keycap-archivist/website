0f0b5a2deabbc40c416b5c1ee4b056b08f9c070c
Vincent LE GOFF <vince.legoff@gmail.com>
2020-05-21 22:54:54 +0200

    lint

--- src/layouts/maker.js
+++ src/layouts/maker.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
