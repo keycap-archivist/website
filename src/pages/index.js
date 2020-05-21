f34c8d62b1ad443600fa7378c26367c53f5f87f7
Nick Christus <nick.christus@icloud.com>
2020-05-20 22:08:49 -0500

    ui_cleanup: working on styles for maker cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,10 +25,13 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <ul className="flex flex-wrap flex-col md:flex-row w-full md:-mx-2">
+      <ul className="flex flex-wrap flex-col md:flex-row w-full md:m-0 md:-mx-1">
         {data.allSitePage.nodes.map((element) => (
-          <li key={element.id} className="flex md:w-1/3 lg:w-1/4 py-3 md:px-2">
-            <Link to={element.path} className="block w-full py-4 font-semibold text-lg text-center border border-l-4">
+          <li key={element.id} className="flex md:w-1/3 lg:w-1/4 py-1 md:px-2">
+            <Link 
+              to={element.path} 
+              className="block w-full py-4 font-semibold text-lg text-center border border-l-4 bg-white"
+            >
               {element.context.maker.name}
             </Link>
           </li>
