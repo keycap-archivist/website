0ed57ebc9e31b3326d35f056b5daabd4ce0b7a99
Nick Christus <136611+nchristus@users.noreply.github.com>
2020-05-23 18:56:33 -0500

    Rwd cleanup (#33)
    * rwd_cleanup: fixing small screen layouts, updated breadcrumbs layout
    
    * rwd_cleanup: added favicons, added copy link button
    
    * Update src/layouts/cw.js
    
    Co-authored-by: Vincent LE GOFF <vince.legoff@gmail.com>
    
    * Update src/layouts/cw.js
    
    Co-authored-by: Vincent LE GOFF <vince.legoff@gmail.com>
    
    Co-authored-by: Vincent LE GOFF <vince.legoff@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,18 +19,6 @@ const plugins = [
   },
   'gatsby-transformer-sharp',
   'gatsby-plugin-sharp',
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: 'gatsby-starter-default',
-      short_name: 'starter',
-      start_url: '/',
-      background_color: '#663399',
-      theme_color: '#663399',
-      display: 'minimal-ui',
-      // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-    },
-  },
   'gatsby-transformer-remark',
 ];
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet-async';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -32,6 +33,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className="container mx-auto px-3 pb-8">
         <main>{children}</main>
