775b52c28df418b04065a4ee9607e5789d55327e
Vincent LE GOFF <vince.legoff@gmail.com>
2020-05-24 17:15:53 +0200

    feat: Improve SEO

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Not found" img={'/android-chrome-512x512.png'} />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
