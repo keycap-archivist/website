6e6f22f660b018c8deb66463f0d2c9b00ae4394e
Vincent LE GOFF <vince.legoff@gmail.com>
2020-05-24 23:35:13 +0200

    feat: add wishlist feature

---
+++ src/pages/wishlist.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+import { getWishlist, rmCap, resetWishlist } from '../internal/wishlist';
+
+const baseAPIurl = 'https://app.keycap-archivist.com/api/v1';
+
+const Wishlist = () => {
+  const [b64Img, setB64Img] = useState(null);
+  const [wishlist, setWishlist] = useState(getWishlist());
+
+  const imgPlaceholder = () => {
+    if (b64Img) {
+      return <img style={{ maxWidth: '500px' }} src={`data:image/jpeg;base64,${b64Img}`} />;
+    }
+    return '';
+  };
+
+  const genWishlist = async () => {
+    const outWishlist = {};
+    outWishlist.ids = wishlist.items.map((x) => x.id);
+    outWishlist.priorities = wishlist.items.filter((x) => x.prio).map((x) => x.id);
+    const b64 = await axios
+      .post(`${baseAPIurl}`, outWishlist, {
+        responseType: 'arraybuffer',
+      })
+      .then((response) => Buffer.from(response.data, 'binary').toString('base64'));
+    setB64Img(b64);
+  };
+
+  const wishlistPlaceHolder = () => (
+    <ul>
+      <button
+        className="bg-green-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-700 rounded"
+        onClick={() => setWishlist(resetWishlist())}
+      >
+        Reset DEV
+      </button>
+      {wishlist.items.map((x) => (
+        <li key={x.id}>
+          {x.id}
+          <img style={{ maxWidth: '200px' }} src={`${baseAPIurl}/img/${x.id}`} />
+          {x.prio ? (
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-700 rounded">
+              remove Priority
+            </button>
+          ) : (
+            <button className="bg-green-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-700 rounded">
+              add Priority
+            </button>
+          )}
+          <button
+            onClick={() => setWishlist(rmCap(x.id))}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-700 rounded"
+          >
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Layout>
+      <SEO title="Wishlist" img={'/android-chrome-512x512.png'} />
+      <h1>Wishlist</h1>
+      {imgPlaceholder()}
+      <button onClick={genWishlist} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        test
+      </button>
+      <p>diz iz da wishlist</p>
+      {wishlistPlaceHolder()}
+    </Layout>
+  );
+};
+
+export default Wishlist;
