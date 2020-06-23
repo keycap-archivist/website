2c24d5bd6c667352bb4f09c38c0fcf46009025da
Nick Christus <nick.christus@icloud.com>
2020-06-23 15:45:15 -0500

    add_about: cleaned up UI a bit, some small text changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,41 +8,42 @@ import SEO from '../components/seo';
 const About = () => (
   <Layout>
     <SEO title="About" />
-    <div className="w-full m-auto lg:w-8/12">
+    <div className="w-full m-auto lg:w-8/12 py-10">
       <h1 className="text-3xl font-bold">About</h1>
-      <p className="text-justify">
-        <strong>Keycap Archivist</strong> is a community driven project for artisan keycap enthusiast for other
-        enthusiasts. This is made in collaboration with{' '}
+      <p className="my-5">
+        <strong>Keycap Archivist</strong> is a community driven project from artisan keycap enthusiasts made for other
+        enthusiasts. It is made in collaboration with{' '}
         <a
           target="_blank"
           rel="noopener noreferrer"
-          className="font-bold hover:text-blue-700"
+          className="font-bold text-blue-700 hover:underline"
+          href="https://keycap.info/"
+        >
+          keycap.info
+        </a>
+        , which is the &quot;single source of truth&quot;. The Google Docs from{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold text-blue-700 hover:underline"
           href="https://keycap.info/"
         >
           keycap.info
         </a>{' '}
-        work; which is the &quot;single source of truth&quot;. The Google Docs from{' '}
+        are dowloaded as HTML and parsed to extract the data as the format is the same on every catalog. Then those data
+        are exposed through an open source repository. Once data is available, we build upon it; that&lsquo;s how{' '}
+        <strong>Keycap Archivist</strong> was born. If you want to contribute just visit the{' '}
         <a
           target="_blank"
           rel="noopener noreferrer"
-          className="font-bold hover:text-blue-700"
-          href="https://keycap.info/"
-        >
-          keycap.info
-        </a>{' '}
-        are dowloaded as HTML and parsed to extract the data as the format is the same on every catalog. By then those
-        data are exposed throught an open source repository. Once Data is available, couple of things can be made upon
-        it; that&lsquo;s how Keycap Archivist was born. If you want to contribute just visit the{' '}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          className="font-bold hover:text-blue-700"
+          className="font-bold text-blue-700 hover:underline"
           href="https://github.com/keycap-archivist/"
         >
           Github
         </a>
-        . The project is composed of the following components:
+        .
       </p>
+      <p className="my-5">The project is composed of the following components:</p>
       <h2 className="text-2xl font-bold">
         Database{' '}
         <a
@@ -54,19 +55,19 @@ const About = () => (
           <FontAwesomeIcon icon={['fab', 'github']} />
         </a>
       </h2>
-      <p className="text-justify">
+      <p className="my-5">
         Database generator is a Javascript based project. Each maker got it&lsquo;s <i>importer</i> module, because even
         if the format is mostly the same there can be some slight differences to handle. All keycap ids are made using a
         CRC32 calculation based on the image url of the keycap.
       </p>
-      <p className="text-justify">
+      <p className="my-5">
         Once the data is collected, it&lsquo;s formated using this json schema and exposed as separated JSON/CSV files
         per maker and one exposing the whole catalog in each format. You can check the compiled database files{' '}
         <a
           href="https://github.com/keycap-archivist/database/tree/master/db"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-blue-700"
+          className="font-bold text-blue-700 hover:underline"
         >
           here
         </a>
@@ -83,10 +84,10 @@ const About = () => (
           <FontAwesomeIcon icon={['fab', 'github']} />
         </a>
       </h2>
-      <p className="text-justify">
+      <p className="my-5">
         The app is a NodeJs server using TypeScript and Fastify. It is mainly used to make the Wishlist you can generate
         in{' '}
-        <Link to="/wishlist" className="hover:text-blue-700">
+        <Link to="/wishlist" className="font-bold text-blue-700 hover:underline">
           here
         </Link>
         . There is also couple of other features that will be described in the Open Api description (soon LMAO). This is
@@ -104,13 +105,13 @@ const About = () => (
           <FontAwesomeIcon icon={['fab', 'github']} />
         </a>
       </h2>
-      <p className="text-justify">
+      <p className="my-5">
         The website you&lsquo;re browsing right now is made using{' '}
         <a
           href="https://github.com/gatsbyjs/gatsby"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-blue-700"
+          className="font-bold text-blue-700 hover:underline"
         >
           Gatsby
         </a>{' '}
@@ -118,14 +119,14 @@ const About = () => (
         every 6 hour and check if there is a different revision. If so it&lsquo;s regenerated and pushed on Github
         Pages.
       </p>
-      <p className="text-justify mt-6">
+      <p className="my-5">
         Thanks for taking the time to read this section, if you want to contribute feel free to raise issues on github
         or come discuss with us on{' '}
         <a
           href="https://discord.gg/nXrShaa"
           target="_blank"
           rel="noopener noreferrer"
-          className="font-bold hover:text-blue-700"
+          className="font-bold text-blue-700 hover:underline"
         >
           discord
         </a>
