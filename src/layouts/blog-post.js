f4ff03319bab6c25eaf285468bff0a46094f3843
Vincent LE GOFF <vince.legoff@gmail.com>
2020-07-08 12:57:01 +0200

    feat: adding coz ITW

--- src/layouts/blog-post.js
+++ src/layouts/blog-post.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Parser } from 'html-to-react';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const Maker = (data) => {
   const htmlToReactParser = new Parser();
   const content = htmlToReactParser.parse(data.pageContext.content);
   return (
     <Layout>
-      <h1>{data.pageContext.title}</h1>
-      <div>{content}</div>
+      <SEO title={data.pageContext.title} description={data.pageContext.description} />
+      <div className="w-full m-auto lg:w-8/12 py-10">
+        <h1 className="text-4xl font-bold mb-5">{data.pageContext.title}</h1>
+        <div className="space-y-6">{content}</div>
+      </div>
     </Layout>
   );
 };
