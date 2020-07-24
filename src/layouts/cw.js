162a27c87216d7cc4ce95ff186cfb0bfe2fb0457
mariodandrea <mariodandrea14@yahoo.com>
2020-07-23 22:57:58 -0400

    Added modal html, modal input and button need css work, need to add functionality to button

--- src/layouts/cw.js
+++ src/layouts/cw.js
@@ -136,6 +136,61 @@ const Maker = (props) => {
           </div>
         </div>
       </div>
+      {showModal && (
+        <>
+          <div
+            className="
+            justify-center
+            items-center
+            flex
+            overflow-x-hidden
+            overflow-y-auto
+            fixed
+            inset-0
+            z-50
+            outline-none
+            focus:outline-none"
+            // onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3xl font-semibold">Suggest Colorway Name</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  {/* <p className="my-4 text-gray-600 text-lg leading-relaxed">
+                    We will work on updating this info as soon as possible
+                  </p> */}
+                  <input></input>
+                  <button
+                    className="
+                    block
+                    w-20
+                    bg-green-500
+                    hover:bg-green-700
+                    text-white
+                    font-bold
+                    py-2 px-3
+                    text-xs
+                    rounded"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
     </Layout>
   );
 };
