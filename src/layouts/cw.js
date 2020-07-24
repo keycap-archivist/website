5fcdda6f4e3d9c9c99c1a2b0f54920c4f6c0552d
mariodandrea <mariodandrea14@yahoo.com>
2020-07-24 15:09:58 -0400

    changed formating to fix linting errors

--- src/layouts/cw.js
+++ src/layouts/cw.js
@@ -166,14 +166,58 @@ const Maker = (props) => {
             focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+              <div
+                className="
+                border-0
+                rounded-lg
+                shadow-lg
+                relative
+                flex
+                flex-col
+                w-full
+                bg-white
+                outline-none
+                focus:outline-none"
+              >
+                <div
+                  className="
+                  flex
+                  items-start
+                  justify-between
+                  p-5 border-b
+                  border-solid
+                  border-gray-300
+                  rounded-t"
+                >
                   <h3 className="text-2xl font-semibold">Suggest Colorway Name</h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="
+                    p-1
+                    ml-auto
+                    bg-transparent
+                    border-0
+                    text-black
+                    opacity-5
+                    float-right
+                    text-3xl
+                    leading-none
+                    font-semibold
+                    outline-none
+                    focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span
+                      className="
+                      bg-transparent
+                      text-black
+                      opacity-5
+                      h-6
+                      w-6
+                      text-2xl
+                      block
+                      outline-none
+                      focus:outline-none"
+                    >
                       ×
                     </span>
                   </button>
