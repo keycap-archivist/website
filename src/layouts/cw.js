669cb0308adc38acb1fdeeb2ee20a9cf3670e723
mariodandrea <mariodandrea14@yahoo.com>
2020-07-26 19:03:46 -0400

    Added modal component, incorporated modal component into cw

--- src/layouts/cw.js
+++ src/layouts/cw.js
@@ -8,6 +8,7 @@ import { getWishlist, isInWishlist, rmCap, addCap } from '../internal/wishlist';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Alert from '../components/alert';
+import Modal from '../components/modal';
 
 const Maker = (props) => {
   const { pageContext, location } = props;
@@ -164,116 +165,14 @@ const Maker = (props) => {
         </div>
       </div>
       {showModal && (
-        <>
-          <div
-            className="
-            modal
-            opacity-100
-            ease-out
-            justify-center
-            items-center
-            flex
-            overflow-x-hidden
-            overflow-y-auto
-            fixed
-            inset-0
-            z-50
-            outline-none
-            focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div
-                className="
-                opacity-0
-                transition-opacity
-                opacity-100
-                ease-out
-                border-0
-                rounded-lg
-                shadow-lg
-                relative
-                flex
-                flex-col
-                w-full
-                bg-white
-                outline-none
-                focus:outline-none"
-              >
-                <div
-                  className="
-                  flex
-                  items-start
-                  justify-between
-                  p-5 border-b
-                  border-solid
-                  border-gray-300
-                  rounded-t"
-                >
-                  <h3 className="text-2xl font-semibold">Suggest Colorway Name</h3>
-                  <button
-                    className="
-                    p-1
-                    ml-auto
-                    bg-transparent
-                    border-0
-                    text-black
-                    opacity-5
-                    float-right
-                    text-3xl
-                    leading-none
-                    font-semibold
-                    outline-none
-                    focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span
-                      className="
-                      bg-transparent
-                      text-black
-                      opacity-5
-                      h-6
-                      w-6
-                      text-2xl
-                      block
-                      outline-none
-                      focus:outline-none"
-                    >
-                      ×
-                    </span>
-                  </button>
-                </div>
-                <div className="relative p-6 flex content-around">
-                  <input
-                    className="suggest__input bg-purple-white shadow rounded border-0 p-2 w-full"
-                    type="search"
-                    placeholder="Name"
-                    onChange={(event) => setSuggestionName(event.target.value)}
-                  ></input>
-                  <button
-                    className="
-                    mx-2
-                    block
-                    w-20
-                    bg-green-500
-                    hover:bg-green-700
-                    text-white
-                    font-bold
-                    py-2 px-3
-                    text-xs
-                    rounded"
-                    onClick={() => {
-                      submitName(colorway.id, suggestionName);
-                      setShowModal(false);
-                    }}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        <Modal
+          modalHeader="Suggest Colorway Name"
+          setModal={setShowModal}
+          setSuggestionName={setSuggestionName}
+          submitName={submitName}
+          colorwayId={colorway.id}
+          suggestionName={suggestionName}
+        ></Modal>
       )}
     </Layout>
   );
