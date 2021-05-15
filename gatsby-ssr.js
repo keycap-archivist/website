28af20f5f68e42af06533ae290a74fe564510d38
Vincent LE GOFF <vince.legoff@gmail.com>
2021-05-15 15:16:30 +0200

    feat: use prefered scheme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -14,6 +14,10 @@ exports.onRenderBody = ({ setHeadComponents }) => {
             if(c.darkMode) {
                 document.documentElement.classList.add('dark');
             }
+        } else {
+          if(!!window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.classList.add('dark');
+          }
         }
     } catch (e) {}
   })();`
--- src/internal/config.js
+++ src/internal/config.js
@@ -9,7 +9,7 @@ const CONSTS = {
 };
 
 const defaultConfig = {
-  darkMode: !!window.matchMedia('(prefers-color-scheme: dark)').matches,
+  darkMode: window && !!window.matchMedia('(prefers-color-scheme: dark)').matches,
   displayMode: CONSTS.displayMode.tiles,
 };
 
