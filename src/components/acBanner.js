9f40f57ccfc4ccbad6659acdddbd77750cf9fa0d
Vincent LE GOFF <vince.legoff@gmail.com>
2023-04-24 16:32:12 +0200

    ac banner

--- src/components/acBanner.js
+++ src/components/acBanner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const AcBanner = () => (
-  <a href="https://artisancollector.com" title="Artisan Collector" target="_blank" rel="noopener noreferrer">
+  <a href="https://forms.gle/vAyGc1p6BgRGw2pe7" title="Artisan Collector" target="_blank" rel="noopener noreferrer">
     <img src="/ACbanner_630.jpg" className="w-full md:hidden" />
-    <img src="/ACbanner_1000.jpg" className="w-full hidden md:inline-flex" />
+    <img src="/ACsurveybanner_2023.jpg" className="w-full hidden md:inline-flex" />
   </a>
 );
 
