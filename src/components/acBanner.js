3f6ede27c66b8c24ef7d22c1a7a392946e69eff0
Vincent Le Goff <vince.legoff@gmail.com>
2023-06-16 17:38:35 +0200

    feat: rollback survey banner (#412)

--- src/components/acBanner.js
+++ src/components/acBanner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const AcBanner = () => (
-  <a href="https://forms.gle/vAyGc1p6BgRGw2pe7" title="Artisan Collector" target="_blank" rel="noopener noreferrer" className="mb-6 block">
+  <a href="https://artisancollector.com" title="Artisan Collector" target="_blank" rel="noopener noreferrer" className="mb-6 block">
     <img src="/ACbanner_630.jpg" alt="Artisan mobile survey banner mobile" className="w-full rounded md:hidden" />
-    <img src="/ACsurveybanner_2023.jpg" className="hidden w-full rounded md:block" alt="Artisan desktop survey banner mobile" />
+    <img src="/ACbanner_1000.jpg" className="hidden w-full rounded md:block" alt="Artisan desktop survey banner mobile" />
   </a>
 );
 
